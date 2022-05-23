71f058602b536863935b0830f049a1d8a6fbd4cb
laskor <al.moksud@gmail.com>
2022-05-22 23:57:51 -0400

    project added

---
+++ src/Components/AddUser/AddUser.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './AddUser.css';
+
+const AddUser = () => {
+    return (
+        <div>
+            <h1>Users</h1>
+        </div>
+    );
+};
+
+export default AddUser;
