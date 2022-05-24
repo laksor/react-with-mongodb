19f1cd0cb4ceef0f756c6f53e0230ad88c7b5407
laskor <al.moksud@gmail.com>
2022-05-24 00:57:45 -0400

    user data showing in ui

--- src/Components/AddUser/AddUser.js
+++ src/Components/AddUser/AddUser.js
@@ -1,12 +1,44 @@
-import React from 'react';
-import './AddUser.css';
+import React, { useState } from "react";
+import "./AddUser.css";
 
 const AddUser = () => {
-    return (
-        <div>
-            <h1>Users</h1>
-        </div>
-    );
+  const [users, setUsers] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const user = {name, email};
+
+    // post data to server
+    fetch("http://localhost:5000/user", {
+      method: "POST", // or 'PUT'
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        const newUser = [...users, data];
+        setUsers(newUser);
+        console.log("Success:", data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
+  return (
+    <div>
+      <h1>PLease add a new user</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" placeholder="Name" required />
+        <input type="email" name="email" placeholder="Email" required />
+        <button type="submit">Add user</button>
+      </form>
+    </div>
+  );
 };
 
 export default AddUser;
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    );
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/user")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+  return (
+    <div>
+      <h1>Home</h1>
+      <ul>
+        {users.map((user) => (
+          <p key={user._id}>
+            name: {user.name} <br></br> email: {user.email}
+          </p>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
